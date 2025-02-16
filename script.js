34ef76b7b2f6f9eb356e0d2d66e44086e1374d00
SOUMYADIP GIRI <122915461+Soumyadipgithub@users.noreply.github.com>
2025-02-16 13:08:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ themeToggleBtn.addEventListener('click', () => {
 
 // Function to update the theme toggle button icon
 function updateThemeIcon(theme) {
-    const iconPath = theme === 'light' ? 'Frontend/public/DarkModeIcon.svg' : 'Frontend/public/LightModeIcon.svg';
+    const iconPath = theme === 'light' ? 'Frontend/public/darkicon.png' : 'Frontend/public/LightIcon.svg';
     themeToggleBtn.querySelector('img').src = iconPath;
 }
 
